7fdd2a84cb5fefe24f323a540fe745bac39a0e33
Girish Krishnanivas <girish.krishnanivas@gmail.com>
2022-09-20 19:04:38 +0100

    commit Program.cs and name-sorter file changes

--- name-sorter/Program.cs
+++ name-sorter/Program.cs
@@ -1,3 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+using name_sorter;
+
+try
+{
+    List<FullName> inputFullNames= null;
+    IEnumerable<FullName> outputFullNames= null;
+    List<IFullNameDataWriter> outputFullNamesWriters= new List<IFullNameDataWriter>();
+
+    try
+    {
+        // Read the input file (filename specfified in args[0]) and get the list of the names as List<FullName>
+        IFullNameDataReader dataReader = new FullNameTextFileReader();
+        inputFullNames = dataReader.GetData(args[0]);        
+    }
+    catch (Exception ex)
+    {
+        // Let the user know what went wrong.
+        Console.WriteLine(ex.Message);
+    }
+
+    if(inputFullNames != null)
+    {
+        if (inputFullNames.Count > 0)
+        {
+            // Sort the names by lastName and then by First Name
+            outputFullNames = inputFullNames.SortNames();
+            if(outputFullNames != null)
+            {
+                // write the sorted output to the file
+                IFullNameDataWriter fileWriter = new FullNameToTextFileWriter("sorted-names-list.txt");
+                outputFullNamesWriters.Add(fileWriter);
+
+                // write the sorted output to the console
+                IFullNameDataWriter ConsoleWriter = new FullNameToConsoleWriter();
+                outputFullNamesWriters.Add(ConsoleWriter);
+
+                try
+                {
+                    // write the sorted list to file and the Console.
+                    foreach (IFullNameDataWriter writer in outputFullNamesWriters)
+                    {
+                        writer.WriteToOutput(outputFullNames);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    } 
+
+}
+catch (Exception ex)
+{
+    // Let the user know what went wrong.
+    Console.WriteLine(ex.Message);
+}
 
--- name-sorter/name-sorter.cs
+++ name-sorter/name-sorter.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
 
 namespace name_sorter
-{
-    public class NameSorter
+{ 
+    
+    /// <summary>
+    /// Helper Class to perform Sorting of the 
+    /// </summary>
+    public static class SortNamesExtension
     {
+        public static IEnumerable<FullName> SortNames(this IEnumerable<FullName> fullNames, IComparer<FullName> comparable)
+        {
+            IEnumerable<FullName> result = fullNames.ToList<FullName>();
+
+            if (comparable != null)
+                ((List<FullName>)result).Sort(comparable);
+            else
+            {
+                // sort by Last Name
+                ((List<FullName>)result).Sort();
+            }
+
+            return result;
+
+        }
+
+        public static IEnumerable<FullName> SortNames(this IEnumerable<FullName> fullNames)
+        {
+            // using LINQ query to orderby LastName and then by Given Name
+            return fullNames.OrderBy(name => name.LastName).ThenBy(name => name.GivenName);
+        }
+
     }
+
+
 }
