cbcf8a97aca7738f796743d57d8a7d754f670f23
Girish Krishnanivas <girish.krishnanivas@gmail.com>
2022-09-20 19:02:34 +0100

    sorting working version 1

---
+++ name-sorter/FullName.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name_sorter
+{
+    /// <summary>
+    ///  structure to represent the name. 
+    /// </summary>
+    public struct FullName : IComparable<FullName>
+    {
+        // Given Name
+        public String GivenName { get; set; }
+
+        // Last Name
+        public String LastName { get; set; }
+
+        /// <summary>
+        ///  Constructor 
+        /// </summary>
+        /// <param name="name">The value to initialize the instance. Atleast one Given Name ( max 3 ) and one Last Name should be specified ("GivenName LstName"). </param>
+        /// <exception cref="ArgumentException"></exception>
+        public FullName(string name)
+        {
+
+
+            if (name.Trim().Length == 0)
+            {
+                // name should have atlest one character, it cannot be empty. throw ArgumentException
+                throw new ArgumentException("Invalid input Empty value provided.");
+            }
+
+            // name is in format "GivenName LastName"
+            // get the last index for ' ' and use it to get the starting pos for the last name.
+            int lastDelimPos = name.Trim().LastIndexOf(' ');
+
+
+            if (lastDelimPos == -1)
+            {
+                // One of GivenName or LastName is missing. throw ArgumentException.
+                throw new ArgumentException($"Invalid format for the entry '{name}'. Format should be 'GivenName LastName'");
+            }
+
+
+            // Given Name is the sequence of characters from 0 to position of last occurance of ' '
+            GivenName = name.Substring(0, lastDelimPos);
+
+            // Last Name is the sequence of characters from (position +1) of last occurance of ' ' till end of string.
+            LastName = name.Substring(lastDelimPos + 1);
+
+            // validate number of given names
+            string[] parts = GivenName.Split(' ');
+            if (parts.Length > 3)
+            {
+                // more than 3 given names found. throw ArgumentException.
+                throw new ArgumentException($" Given Names count > 3. Invalid input :  '{name}'");
+            }
+        }
+
+        /// <summary>
+        ///  default construction. Initializes the GivenName and LastName to Empty String.
+        /// </summary>
+        public FullName()
+        {
+            GivenName = String.Empty;
+            LastName = String.Empty;
+        }
+
+        /// <summary>
+        ///  Method to provide an CompareTo implementation to the struct Full Name. 
+        ///  Needed to support Sort() of Collection classes like List,Array etc. 
+        /// </summary>
+        /// <param name="obj"> An object to compare with this instance.</param>
+        /// <returns> 
+        ///     A value that indicates the relative order of the objects being compared. The
+        ///     return value has these meanings:
+        ///     Value – Meaning
+        ///     Less than zero – This instance precedes other in the sort order.
+        ///     Zero – This instance occurs in the same position in the sort order as other.
+        ///     Greater than zero – This instance follows other in the sort order.
+        ///  </returns>
+        int IComparable<FullName>.CompareTo(FullName obj)
+        {
+
+            int nCompareLastName = String.Compare(LastName, obj.LastName, true);
+
+            if (nCompareLastName == 0)
+            {
+                return String.Compare(GivenName, obj.GivenName, true);
+            }
+
+            return nCompareLastName;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (String.Compare(this.ToString(), obj.ToString(), true) == 0)
+                return true;
+
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        ///  String representation of the instance. Returns "GivenName LastName"
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return GivenName + " " + LastName;
+        }
+
+        /// <summary>
+        /// Generate the HashCode for the instance.
+        /// </summary>
+        /// <returns>integer value</returns>
+        public override int GetHashCode()
+        {
+            return GivenName.GetHashCode() + LastName.GetHashCode();
+        }
+    }
+}
---
+++ name-sorter/FullNameDataManager.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace name_sorter
+{
+    /// <summary>
+    ///  Read FullNames data from a file in the file system
+    /// </summary>
+    public interface IFullNameDataReader
+    {
+        public List<FullName> GetData(string fileName);
+    }
+
+    /// <summary>
+    ///  Write FullNames data from a file in the file system
+    /// </summary>
+    public interface IFullNameDataWriter
+    {
+        /// <summary>
+        /// write the data to the output destination
+        /// </summary>
+       /// <param name="names">list of names to be written to the file</param>
+        public void WriteToOutput(IEnumerable<FullName> names);
+    }
+
+
+    /// <summary>
+    ///  To write data to file in a .txt format
+    /// </summary>
+    public class FullNameToTextFileWriter : IFullNameDataWriter
+    {
+        public string FileName { get; set; }
+
+        public FullNameToTextFileWriter()
+        {
+            FileName = "output-list.txt";
+        }
+
+        public FullNameToTextFileWriter(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        /// <summary>
+        /// Write the list of the names to the .txt file specified in FileName property.
+        /// </summary>
+        /// <param name="names"></param>
+        /// <exception cref="InvalidDataException"> Exception while writing output to the file. Inner Exception will have more details</exception>
+        void IFullNameDataWriter.WriteToOutput(IEnumerable<FullName> names)
+        {
+            try
+            {
+                
+                FileStreamOptions options = new FileStreamOptions();
+                options.Access = FileAccess.Write;
+                options.Mode = FileMode.Create;
+
+                // Create an instance of StreamWriter to write to a file.
+                // The using statement also closes the StreamWriter.
+                using (StreamWriter writer = new StreamWriter(FileName, options))
+                {
+                    foreach (FullName name in names)
+                    {
+                        // write the name to the file.
+                        writer.WriteLine(name.ToString());
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidDataException($" Failed to write the data to file {FileName} .", ex);
+            }
+
+        }
+    }
+
+
+    /// <summary>
+    ///  To write the data to the output Console 
+    /// </summary>
+    public class FullNameToConsoleWriter : IFullNameDataWriter
+    {        
+        /// <summary>
+        ///  write the names  to the output console.
+        /// </summary>
+        /// <param name="names"></param>
+        void IFullNameDataWriter.WriteToOutput(IEnumerable<FullName> names)
+        {
+            foreach (FullName name in names)
+            {
+                // write the name to the console screen.
+               Console.WriteLine(name.ToString());
+            }            
+
+        }
+    }
+
+    /// <summary>
+    ///  To read data from a .txt file
+    /// </summary>
+    public class FullNameTextFileReader : IFullNameDataReader
+    {
+        /// <summary>
+        ///  Read the data from a .txt file.
+        /// </summary>
+        /// <param name="fileName">Name of the file</param>
+        /// <returns>Collection of FullName instances. Collection is empty if any excepion occurs.</returns>
+        /// <exception cref="InvalidDataException"> Exception while reading names from the file. Inner Exception will have more details.</exception>
+        List<FullName> IFullNameDataReader.GetData(string fileName)
+        {
+            List<FullName> fullNames = new List<FullName>();
+
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    // Create an instance of StreamReader to read from a file.
+                    // The using statement also closes the StreamReader.
+                    using (StreamReader reader = new StreamReader(fileName))
+                    {
+                        string line;
+
+                        // Read lines from the file until the end of
+                        // the file is reached.
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // ignore empty new line entries
+                            if (line.Trim().Length > 0)
+                                fullNames.Add(new FullName(line));
+                        }
+                    }
+                }
+                else
+                {
+                    throw new FileNotFoundException($"File Not found : '{fileName}'");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                fullNames.Clear();
+                throw new InvalidDataException($" Failed to read the data from file {fileName} .", ex);
+            }
+
+            return fullNames;
+
+        }
+    }
+}
